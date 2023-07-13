e369ce5540311583b1df30b6598fe9e4eea77cc5
Gloriariechi99 <gloriariechi99@gmail.com>
2023-07-12 23:46:11 -0700

    betty

---
+++ getCard.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - get number of cards from user and show pid
+ * Return: Always zero
+ */
+int main(void)
+{
+	int decks;
+	pid_t pid;
+	pid_t ppid;
+
+	pid = getpid();
+	ppid = getppid();
+
+	puts("Enter a number of decks");
+	scanf("%i", &decks);
+	if (decks < 1)
+	{
+		puts("This is not a valid number of decks");
+		return (1);
+	}
+	printf("There are %i cards\n", decks * 52);
+	printf("The pid is %u\n", pid);
+	printf("The ppid is %u\n", ppid);
+
+	return (0);
+}
